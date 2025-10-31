7db978398c432cbe57a2d16e8a28d693cff18dd6
Shubham kahar <Shubhamkahar196@gmail.com>
2025-10-31 10:40:29 +0530

    Added district/route.ts

---
+++ app/api/data/district/route.ts
@@ -0,0 +1,44 @@
+import { NextRequest, NextResponse } from "next/server";
+import {prisma} from '@/app/lib/database'
+
+export async function GET(request:NextRequest){
+    try {
+        const {searchParams} = new URL(request.url);
+        const districtId = searchParams.get('districtId');
+        const year = searchParams.get('year');
+
+        if(!districtId){
+            return NextResponse.json(
+                {success: false, error: 'District ID Requied'},
+                {status: 400}
+            );
+        }
+
+        const whereClause: any = {districtId};
+        if(year){
+            whereClause.year = parseInt(year);
+        }
+
+        const performance = await prisma.performance.findMany({
+            where: whereClause,
+            orderBy: [
+                {year: 'desc'},
+            {month: 'desc'}
+            ],
+            include: {
+                district: true
+            }
+        });
+
+        return NextResponse.json({
+            success: true,
+            data: performance
+        });
+    } catch (error) {
+        console.error('Data fetch error : ',error);
+        return NextResponse.json(
+            {success: false, error: 'Failed to fetch data'},
+            {status: 500}
+        )
+    }
+}
